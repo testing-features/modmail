67f8b55844e4f208dfca5cff0ecff3bd8cbe5048
onerandomusername <genericusername414+git@gmail.com>
2021-08-30 01:08:18 -0400

    minor: don't use the global ext/plug lists

--- tests/modmail/extensions/test_plugin_manager.py
+++ tests/modmail/extensions/test_plugin_manager.py
@@ -3,15 +3,13 @@ from copy import copy
 import pytest
 
 from modmail.extensions.plugin_manager import PluginConverter
-from modmail.utils.plugins import PLUGINS, walk_plugins
-
-# load EXTENSIONS
-PLUGINS = copy(PLUGINS)
-PLUGINS.update(walk_plugins())
+from modmail.utils.plugins import walk_plugins
 
 
 class TestExtensionConverter:
     """Test the extension converter converts extensions properly."""
+
+    all_plugins = {x: y for x, y in walk_plugins()}
 
     @pytest.fixture(scope="class", name="converter")
     def converter(self) -> PluginConverter:
@@ -19,10 +17,10 @@ class TestExtensionConverter:
         return PluginConverter()
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize("extension", [e.rsplit(".", 1)[-1] for e in PLUGINS.keys()])
+    @pytest.mark.parametrize("extension", [e.rsplit(".", 1)[-1] for e in all_plugins.keys()])
     async def test_conversion_success(self, extension: str, converter: PluginConverter) -> None:
         """Test all extensions in the list are properly converted."""
-        converter.source_list = PLUGINS
+        converter.source_list = self.all_plugins
         converted = await converter.convert(None, extension)
 
         assert converted.endswith(extension)
